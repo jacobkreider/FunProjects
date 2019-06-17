463abfa4e1d80fa0b372374b0fd68390f6c53b8c
jacobkreider <36924351+jacobkreider@users.noreply.github.com>
2019-06-16 20:58:07 -0400

    Forgot the vowel rule in Pig Latin-- added to code

--- ImpracticalPython/Chapter 1/pig_latin_practice_project.py
+++ ImpracticalPython/Chapter 1/pig_latin_practice_project.py
@@ -7,14 +7,20 @@ def main():
     """
     print("Welcome to the Pig Latin Translator\n\n")
     english_string = input("What word would you like translated?\n\n")
+    vowels = ['a', 'e', 'i', 'o', 'u', 'y']
 
-    pig_suffix = "ay"
+    pig_suffix = 'ay'
     new_ending = english_string[0]
 
-    print("{} is translated as {}{}{}".format(english_string
-                                              , english_string[1j:]
+    if english_string[0] in vowels:
+        print("{} is translated as {}{}".format(english_string
+                                              , english_string, pig_suffix))
+    else:
+        print("{} is translated as {}{}{}".format(english_string
+                                              , english_string[1:]
                                               , new_ending, pig_suffix))
 
 
 if __name__ == "__main__":
     main()
+
